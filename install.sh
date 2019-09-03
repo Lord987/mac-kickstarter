9e1c7386a787a3c1afddbc4d794a4c8dc6fac355
Lord987 <Lord987@users.noreply.github.com>
2019-09-03 14:23:52 +0200

    Init

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install bash-completion
+echo "[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion" >> ~/.bashrc
+
+brew install kubectl
+kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
+
+cd /usr/local/etc/bash_completion.d \
+    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion \
+    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion \
+    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+
+brew cask install emacs
+brew install httpie
+brew install pass
