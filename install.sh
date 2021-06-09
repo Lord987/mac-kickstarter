c9a88ce70a5ce9e0cfa6a5da010a495e79f5b554
Lord987 <Lord987@users.noreply.github.com>
2021-06-09 16:53:39 +0200

    update

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install bash-completion
+
+# Not used anymore with zsh
+#brew install bash-completion
 #echo "[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion" >> ~/.bashrc
 
 brew install kubernetes-cli
 #kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
 
-cd /usr/local/etc/bash_completion.d \
-    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion \
-    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion \
-    && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+# Not used anymore with zsh
+# cd /usr/local/etc/bash_completion.d \
+#     && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion \
+#     && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion \
+#     && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
 
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
+# Not used when installing with homebrew
+# ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
+# ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
+# ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
 
-brew cask install emacs tunnelblick ajour altair-graphql-client postman
-#brew cask install vivaldi
-brew install httpie pass go zsh-completion kubectx kube-ps1 kustomize aws-iam-authenticator awscli drone-cli helm istioctl mongodb-community-shell \
-    conftest kubeval yamllint opa sops
+brew cask install emacs tunnelblick ajour altair-graphql-client postman docker vivaldi
+brew install httpie pass go zsh-completions kubectx kube-ps1 kustomize aws-iam-authenticator awscli drone-cli helm istioctl mongodb-community-shell \
+    mongodb-database-tools conftest kubeval yamllint opa sops flux jq
 #brew install node kops
