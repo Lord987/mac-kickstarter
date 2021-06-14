707f2bbc33c0a245e731e356f870b827a3ca1c71
Lord987 <Lord987@users.noreply.github.com>
2021-06-14 17:20:37 +0200

    add discord and slack

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ brew install kubernetes-cli
 brew tap mongodb/brew
 brew tap instrumenta/instrumenta
 
-brew install --cask emacs tunnelblick ajour altair-graphql-client postman docker vivaldi visual-studio-code
+brew install --cask emacs tunnelblick ajour altair-graphql-client postman docker vivaldi visual-studio-code slack discord
 brew install httpie pass go zsh-completions kubectx kube-ps1 kustomize aws-iam-authenticator awscli drone-cli helm istioctl mongodb-community-shell \
     mongodb-database-tools conftest kubeval yamllint opa sops flux jq
 #brew install node kops
